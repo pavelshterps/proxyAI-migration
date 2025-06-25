eb30679eb32a46bfb8bf3170568a7224b97b3742
pavelshterps <pavelshterps@gmail.com>
2025-06-25 21:35:46 +0300

    big change 7.9.6

--- celery_app.py
+++ celery_app.py
@@ -2,21 +2,18 @@
 
 from config.settings import settings
 from celery import Celery
-import tasks
 
-# Инициализируем Celery, указывая явно, что нужно импортировать наш модуль tasks
-celery_app = Celery(
+# Инициализируем Celery без include/imports
+app = Celery(
     __name__,
     broker=settings.CELERY_BROKER_URL,
     backend=settings.CELERY_RESULT_BACKEND,
-    include=["tasks"],       # <- вот это добавлено
 )
 
 # Автоматически находим задачи в модуле tasks
-celery_app.autodiscover_tasks(["tasks"])  # <- и вот это
-celery_app.conf.imports = ["tasks"]
+app.autodiscover_tasks(["tasks"])
 # Настраиваем маршрутизацию по очередям
-celery_app.conf.task_routes = {
+app.conf.task_routes = {
     # полный файл для диаризации идёт в очередь preprocess_cpu
     "tasks.diarize_full": {"queue": "preprocess_cpu"},
     # чанки для транскрипции — в очередь preprocess_gpu
@@ -24,6 +21,6 @@ celery_app.conf.task_routes = {
 }
 
 # Опционально можно задать дефолтную очередь
-celery_app.conf.task_default_queue = "preprocess_cpu"
-celery_app.conf.task_default_exchange = "preprocess_cpu"
-celery_app.conf.task_default_routing_key = "preprocess_cpu"
+app.conf.task_default_queue = "preprocess_cpu"
+app.conf.task_default_exchange = "preprocess_cpu"
+app.conf.task_default_routing_key = "preprocess_cpu"
