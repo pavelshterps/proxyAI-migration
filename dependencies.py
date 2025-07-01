7b6f7fe8d64d12fa6340547352cb2108d9d5d0f0
pavelshterps <pavelshterps@gmail.com>
2025-07-01 11:50:32 +0300

    13.8.4.3

--- dependencies.py
+++ dependencies.py
@@ -1,3 +1,11 @@
+from fastapi import HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.security import APIKeyHeader
+from database import get_db
+from crud import get_user_by_api_key
+
+api_key_header = APIKeyHeader(name="X-API-Key")
+
 async def get_current_user(
         key: str = Depends(api_key_header),
         db: AsyncSession = Depends(get_db)
