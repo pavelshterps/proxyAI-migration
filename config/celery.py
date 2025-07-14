44dd304495b7a4b482437858c073a45971c6fc7e
pavelshterps <pavelshterps@gmail.com>
2025-07-14 15:41:46 +0300

    21.9.9

--- config/celery.py
+++ config/celery.py
@@ -1,6 +1,26 @@
-from kombu import Queue
+# config/celery.py
 
+from celery import Celery
+from kombu import Queue
+from config.settings import settings
+
+# 1) Сначала создаём приложение
+celery_app = Celery(
+    'proxyai',
+    broker=settings.CELERY_BROKER_URL,
+    backend=settings.CELERY_RESULT_BACKEND,
+    timezone=settings.CELERY_TIMEZONE,
+    include=['tasks'],
+)
+
+# 2) Затем настраиваем его
 celery_app.conf.update(
+    task_serializer='json',
+    accept_content=['json'],
+    result_serializer='json',
+    broker_url=settings.CELERY_BROKER_URL,
+    result_backend=settings.CELERY_RESULT_BACKEND,
+
     task_queues=[
         Queue('transcribe_cpu'),
         Queue('transcribe_gpu'),
@@ -8,7 +28,6 @@ celery_app.conf.update(
     ],
     task_routes={
         'tasks.preview_slice':      {'queue': 'transcribe_cpu'},
-        'tasks.preview_whisper':    {'queue': 'transcribe_gpu'},
         'tasks.transcribe_segments':{'queue': 'transcribe_gpu'},
         'tasks.diarize_full':       {'queue': 'diarize_gpu'},
     },
